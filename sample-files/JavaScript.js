566d0bca3925de8ed059b294ae6055c8289fe7b4
Jesse Weed <jesse@jesseweed.com>
2015-02-14 23:03:52 -0800

    add sample files

--- sample-files/JavaScript.js
+++ sample-files/JavaScript.js
@@ -1,93 +1,28 @@
-/**
- * Module dependencies.
- */
+var koa = require('koa');
+var app = koa();
 
-var EventEmitter = require('events').EventEmitter;
-var mixin = require('merge-descriptors');
-var proto = require('./application');
-var Route = require('./router/route');
-var Router = require('./router');
-var req = require('./request');
-var res = require('./response');
+// x-response-time
 
-/**
- * Expose `createApplication()`.
- */
-
-exports = module.exports = createApplication;
-
-/**
- * Create an express application.
- *
- * @return {Function}
- * @api public
- */
-
-function createApplication() {
-  var app = function(req, res, next) {
-    app.handle(req, res, next);
-  };
-
-  mixin(app, proto);
-  mixin(app, EventEmitter.prototype);
-
-  app.request = { __proto__: req, app: app };
-  app.response = { __proto__: res, app: app };
-  app.init();
-  return app;
-}
-
-/**
- * Expose the prototypes.
- */
-
-exports.application = proto;
-exports.request = req;
-exports.response = res;
-
-/**
- * Expose constructors.
- */
-
-exports.Route = Route;
-exports.Router = Router;
-
-/**
- * Expose middleware
- */
-
-exports.query = require('./middleware/query');
-exports.static = require('serve-static');
-
-/**
- * Replace removed middleware with an appropriate error message.
- */
-
-[
-  'json',
-  'urlencoded',
-  'bodyParser',
-  'compress',
-  'cookieSession',
-  'session',
-  'logger',
-  'cookieParser',
-  'favicon',
-  'responseTime',
-  'errorHandler',
-  'timeout',
-  'methodOverride',
-  'vhost',
-  'csrf',
-  'directory',
-  'limit',
-  'multipart',
-  'staticCache',
-].forEach(function (name) {
-  Object.defineProperty(exports, name, {
-    get: function () {
-      throw new Error('Most middleware (like ' + name + ') is no longer bundled with Express and must be installed separately. Please see https://github.com/senchalabs/connect#middleware.');
-    },
-    configurable: true
-  });
+app.use(function *(next){
+  var start = new Date();
+  yield next;
+  var ms = new Date() - start;
+  this.set('X-Response-Time', ms + 'ms');
 });
+
+// logger
+
+app.use(function *(next){
+  var start = new Date();
+  yield next;
+  var ms = new Date() - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+// response
+
+app.use(function *(){
+  this.body = 'Hello World';
+});
+
+app.listen(3000);
